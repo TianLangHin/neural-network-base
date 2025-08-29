762e6b5cfe4464ff547e250b833365ba8d975e6e
TianLangHin <tianlanghin@gmail.com>
2025-08-29 23:47:04 +1000

    Added functionality of drawing ROC curve and calculating ROC-AUC

--- evaluation/metrics.py
+++ evaluation/metrics.py
@@ -1,3 +1,4 @@
+from typing import List, Tuple
 import numpy as np
 
 def binarise(x: np.ndarray, *, threshold: float = 0.5) -> np.ndarray:
@@ -23,3 +24,36 @@ def f1_score(y_true: np.ndarray, y_pred: np.ndarray, *, threshold: float = 0.5) 
     p = precision(y_true, y_pred, threshold=threshold)
     r = recall(y_true, y_pred, threshold=threshold)
     return 2 / (1/p + 1/r)
+
+def roc(y_true: np.ndarray, y_prob: np.ndarray, *, intervals: int) -> List[Tuple[float, float]]:
+    thresholds = np.arange(intervals + 1) / intervals
+    points = []
+    for threshold in thresholds:
+        y_pred = binarise(y_prob, threshold=threshold)
+        tp = ((y_true == 1) & (y_pred == 1)).sum()
+        tn = ((y_true == 0) & (y_pred == 0)).sum()
+        fp = ((y_true == 0) & (y_pred == 1)).sum()
+        fn = ((y_true == 1) & (y_pred == 0)).sum()
+        sensitivity = float(tp / (tp + fn))
+        specificity = float(tn / (tn + fp))
+        points.append((1 - specificity, sensitivity))
+    return points
+
+def curve_points(points: List[Tuple[float, float]]) -> List[Tuple[float, float]]:
+    i = 1
+    while i < len(points):
+        if points[i][0] == points[i-1][0]:
+            points = points[:i] + points[i+1:]
+        else:
+            i += 1
+    if points[-1] != (0.0, 0.0):
+        points.append((0.0, 0.0))
+    return points
+
+def roc_auc(y_true: np.ndarray, y_prob: np.ndarray, *, intervals: int) -> float:
+    points = roc(y_true, y_prob, intervals=intervals)
+    unique_points = curve_points(points)[::-1]
+    area = 0
+    for previous_point, next_point in zip(unique_points[:-1], unique_points[1:]):
+        area += 0.5 * (next_point[0] - previous_point[0]) * (next_point[1] + previous_point[1])
+    return area
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from collections import namedtuple
-from typing import List
+from typing import List, Tuple
 import functools
 import matplotlib.pyplot as plt
 import numpy as np
@@ -7,7 +7,7 @@ import sys
 
 from activation import ReLU, Sigmoid
 from data import BatchLoader, MinMaxScaler, ZScoreScaler, load_dataset, train_test_split
-from evaluation.metrics import accuracy, f1_score, precision, recall
+from evaluation.metrics import accuracy, f1_score, precision, recall, roc, roc_auc, curve_points
 from evaluation.validation import k_fold_cross_val_scores
 from loss import BinaryCrossEntropy
 from model import Layer, NeuralNetwork
@@ -16,7 +16,7 @@ from optimiser import AdamOptimiser, NaiveOptimiser, Optimiser
 HyperParamsNN = namedtuple('HyperParamsNN', ['layers', 'seed'])
 HyperParamsTrain = namedtuple('HyperParamsTrain', ['lr', 'loss_fn', 'optim_type', 'batch_size', 'epochs'])
 
-Metrics = namedtuple('Metrics', ['accuracy', 'precision', 'recall', 'f1'])
+Metrics = namedtuple('Metrics', ['accuracy', 'precision', 'recall', 'f1', 'auc'])
 
 def make_estimator(
         x_train: np.ndarray,
@@ -42,13 +42,54 @@ def make_estimator(
 
     return nn
 
-def score(nn: NeuralNetwork, x: np.ndarray, y_true: np.ndarray) -> Metrics:
+def score_estimator(nn: NeuralNetwork, x: np.ndarray, y_true: np.ndarray) -> Metrics:
     y_pred = nn.forward(x)
     return Metrics(
         accuracy(y_true, y_pred),
         precision(y_true, y_pred),
         recall(y_true, y_pred),
-        f1_score(y_true, y_pred))
+        f1_score(y_true, y_pred),
+        roc_auc(y_true, y_pred, intervals=20))
+
+def k_fold_roc_curves(seed: int):
+    def roc_points(nn: NeuralNetwork, x: np.ndarray, y_true: np.ndarray) -> List[Tuple[float, float]]:
+        return roc(y_true, nn.forward(x), intervals=20)
+    x, y_true = load_dataset()
+
+    layers = [
+        Layer(50, ReLU()),
+        Layer(20, ReLU()),
+        Layer(10, ReLU()),
+        Layer(1, Sigmoid())
+    ]
+    scaler = MinMaxScaler(x.shape[1], 0, 1)
+
+    k_folds = 5
+    lr = 0.001
+    batch_size = 32
+    epochs = 1500
+    loss_fn = BinaryCrossEntropy()
+
+    estimator = functools.partial(
+        make_estimator,
+        nn_hyperparams=HyperParamsNN(layers, seed),
+        train_hyperparams=HyperParamsTrain(lr, loss_fn, AdamOptimiser, batch_size, epochs))
+    score_list = k_fold_cross_val_scores(estimator, roc_points, scaler, x=x, y=y_true, k=k_folds, verbose=False)
+
+    curves = [curve_points(points) for points in score_list]
+
+    plt.figure()
+    for i, curve in enumerate(curves):
+        curve.reverse()
+        area = 0
+        for prev_point, next_point in zip(curve[:-1], curve[1:]):
+            area += 0.5 * (next_point[0] - prev_point[0]) * (next_point[1] + prev_point[1])
+        plt.plot(
+            [point[0] for point in curve],
+            [point[1] for point in curve],
+            label=f'Fold {i+1}, AUC: {area:.4f}')
+    plt.legend()
+    plt.savefig('roc.png')
 
 def main(seed: int):
     x, y_true = load_dataset()
@@ -76,9 +117,10 @@ def main(seed: int):
     score_mean = np.array(scores).mean(axis=0)
     score_std = np.array(scores).std(axis=0)
     print('Mean:')
-    print('  Accuracy: {0:.4f} Precision: {1:.4f} Recall: {2:.4f} F1-Score: {3:.4f}'.format(*score_mean))
+    print('  Accuracy: {0:.4f} Precision: {1:.4f} Recall: {2:.4f} F1-Score: {3:.4f} AUC: {4:.4f}'.format(*score_mean))
     print('Standard Deviation:')
-    print('  Accuracy: {0:.4f} Precision: {1:.4f} Recall: {2:.4f} F1-Score: {3:.4f}'.format(*score_std))
+    print('  Accuracy: {0:.4f} Precision: {1:.4f} Recall: {2:.4f} F1-Score: {3:.4f} AUC: {4:.4f}'.format(*score_std))
 
 if __name__ == '__main__':
-    main(int(sys.argv[1]))
+    # main(int(sys.argv[1]))
+    k_fold_roc_curves(int(sys.argv[1]))
